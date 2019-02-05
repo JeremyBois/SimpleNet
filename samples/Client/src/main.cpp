5965c9fc83d8e36156b42d218da108e8ea66e61c
JeremyBois <jeremybois@protonmail.com>
2019-02-05 09:40:06 +0100

    Add Cereal lib for make serialization easier and cross platform. Temp test in client.

--- samples/Client/src/main.cpp
+++ samples/Client/src/main.cpp
@@ -1,12 +1,63 @@
 #include "simpleNET.hpp"
 #include <iostream>
 
+#include <cereal/archives/binary.hpp>
+#include <sstream>
+
+
+
+
+struct MyClass
+{
+    public:
+        int x, y, z;
+    // This method lets cereal know which data members to serialize
+    template<class Archive>
+    void serialize(Archive & archive)
+    {
+      archive( x, y, z ); // serialize things by passing them to the archive
+    }
+};
+
+void TestCereal()
+{
+    std::cout << "\nSTART Cereal Test" << std::endl;
+    // Test Cereal
+    std::stringstream ss; // any stream can be used
+
+    // RAII --> Data guaranteed  to flush on destruction --> create scope to force destruction
+    {
+        cereal::BinaryOutputArchive oarchive(ss); // Create an output archive
+
+        MyClass m1, m2, m3;
+        m1.x = 23;
+        m2.y = 444;
+
+        oarchive(m1, m2, m3); // Write the data to the archive
+    } // archive goes out of scope, ensuring all contents are flushed
+
+    {
+        cereal::BinaryInputArchive iarchive(ss); // Create an input archive
+
+        MyClass m1, m2, m3;
+        iarchive(m1, m2, m3); // Read the data from the archive
+
+        std::cout << m1.x << " -- " << m2.y << std::endl;
+    }
+
+    std::cout << "END Cereal Test \n" << std::endl;
+}
+
 
 namespace Net = simpleNET;
 
 
 int main(int argc, char **argv)
 {
+    // Test Cereal
+    TestCereal();
+
+
     // Init
     Net::SimpleNetInit();
 
